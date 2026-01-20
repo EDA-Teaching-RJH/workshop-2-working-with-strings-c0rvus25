9c937317417f00f5b96c60bcbc4d0391b2364eb8
c0rvus25 <imv1@kent.ac.uk>
2026-01-20 12:43:09 +0000

    completed task one

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,11 @@
 import math  
 
 def main():
-#TO DO  
-
+    A = float(input("Input A: "))
+    B = float(input("Input B: "))
+    C = pythag(A,B)
+    print("Hypotenuse that you're after is: " , C)
 def pythag(A,B):
-#TO DO  
+    return math.sqrt(A**2 + B**2)
 
 main()
